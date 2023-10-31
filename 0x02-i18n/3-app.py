b437bdd80759af443282de97f38a14f922ed1235
Ambesawi <ambesawi77@gmail.com>
2023-10-31 20:31:18 +0000

    3-app.py

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+A Basic flask application
+"""
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    Application configuration class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Instantiate the application object
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Wrap the application with Babel
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
