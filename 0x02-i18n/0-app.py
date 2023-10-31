78392aafcb46aa36b080e551babccc92c17b0398
Ambesawi <ambesawi77@gmail.com>
2023-10-31 20:00:41 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,14 +2,20 @@
 """
 A Basic flask application
 """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
-
