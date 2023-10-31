3b3fc564282d6707727e950b6f4ae13b5ec2c6aa
Ambesawi <ambesawi77@gmail.com>
2023-10-31 20:25:40 +0000

    1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+A Basic flask application
+"""
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    Application configuration class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Instantiate the application object
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Wrap the application with Babel
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
