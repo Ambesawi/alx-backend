6475c65a05db85fa3a5f76ab6d8cb7dda9c77cc0
Ambesawi <ambesawi77@gmail.com>
2023-11-30 00:08:31 +0300

    1-redis_op.js

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+/**
+ * Connect to redis server via redis client
+ */
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.SET(schoolName, value, print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.GET(schoolName, (err, value) => {
+    console.log(value);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
