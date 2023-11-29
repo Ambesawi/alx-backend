a30199a50d0871511cd1a2147cf1668a9cd86562
Ambesawi <ambesawi77@gmail.com>
2023-11-30 00:07:33 +0300

    0-redis_client.js

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * Connect to redis server via redis client
+ */
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
